5b98de8ed65fb4869b3ea9d586c7369522132a58
ericly <ericlyeric@gmail.com>
2023-02-15 22:55:41 -0800

    fix helper components

--- src/ProgressBar.tsx
+++ src/ProgressBar.tsx
@@ -1,19 +1,14 @@
-import { useMemo } from "react";
-
 interface ProgressBarProps {
-  currentValue: number;
-  maxValue: number;
+  percentage: number;
 }
 
-export const ProgressBar = ({ currentValue, maxValue }: ProgressBarProps) => {
-  const percentage = useMemo(() => {
-    return (currentValue / maxValue) * 100;
-  }, [currentValue, maxValue]);
-
+export const ProgressBar = ({ percentage }: ProgressBarProps) => {
   return (
     <div className="flex w-full bg-gray-200 rounded-full dark:bg-gray-700">
       <div
-        className="bg-red-700 text-xs font-medium h-4 text-amber-100 text-center p-0.5 leading-none rounded-full"
+        className={`${
+          percentage > 0 ? "bg-red-700" : ""
+        } text-xs font-medium h-4 text-amber-100 text-center p-0.5 leading-none rounded-full`}
         style={{ width: `${percentage}%` }}
       />
     </div>
--- src/helper.ts
+++ src/helper.ts
@@ -8,6 +8,7 @@ export const convertTextToDecimal = (number: string) => {
 };
 
 export const roundDecimalPlaces = (value: number, decimals: number) => {
+  if (!Number.isFinite(value)) return 0;
   const factor = Math.pow(10, decimals);
   return Math.round(value * factor) / factor;
 };
